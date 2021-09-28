4eca5db3243905956541addbe6b40226eda3d465
tc.wang <tc.wang@tcwangdeMacBook-Air.local>
2021-09-28 11:25:54 +0800

    Update config files

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="p-6">
-    <button class="bg-blue-500 rounded-lg px-6 py-3 leading-none text-white uppercase font-semibold text-sm shadow-sm hover:shadow-lg transition-all duration-200 transform hover:-translate-y-0.5">Sign up</button>
+    <button
+      class="bg-blue-500 rounded-lg px-6 py-3 leading-none text-white uppercase font-semibold text-sm shadow-sm hover:shadow-lg transition-all duration-200 transform hover:-translate-y-0.5"
+    >
+      Sign up
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style src="./assets/styles/app.css" />
